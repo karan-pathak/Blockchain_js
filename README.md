9b037b1fa3d117ae439265ea74831b31cf470f87
karan-pathak <karan150394@gmail.com>
2018-07-31 00:43:19 +0530

    corrections and add more functions

---
+++ README.md
@@ -0,0 +1,6 @@
+# Basic BlockChain Implementation in Javascript
+This is a very basic implementation of blockchain in javascript using `js-sha256` library.
+
+# Run
+* Run `npm install`
+* Execute main file using `node main.js`
--- main.js
+++ main.js
@@ -1,27 +1,12 @@
 const SHA256 = require('js-sha256').sha256;
 
 class Block{
-  constructor(transaction, chain, genesis=false){
-    if(genesis){
-      this.index = 0;
-      this.previousHash = 0;
-    }
-    else{
-      this.index = this.getIndexFromChain(chain);
-      this.previousHash = this.getPreviousHash(chain);
-    }
+  constructor(transaction, index = '', previousHash = ''){
+    this.index = index;
+    this.previousHash = previousHash;
     this.transaction = transaction;
     this.timestamp = this.getTimestamp();
     this.hash = this.createHash();
-  }
-
-  getIndexFromChain(blockChain, genesis){
-      blockChain.getLength();
-  }
-
-  getPreviousHash(blockChain){
-      let previousBlock = blockChain.getLatestBlock();
-      return previousBlock.hash;
   }
 
   getTimestamp(){
@@ -40,7 +25,7 @@ class BlockChain{
   }
 
   createGenesisBlock(){
-    return new Block("N/A", this, true);
+    return new Block("N/A", 0, 0);
   }
 
   getLength(){
@@ -52,12 +37,41 @@ class BlockChain{
     return this.chain[chainLength-1];
   }
 
-  addBlock(transaction){
-    let block = new Block(transaction, this);
+  getPreviousHash(){
+      let previousBlock = this.getLatestBlock();
+      return previousBlock.hash;
+  }
+
+  addBlock(block){
+    block.previousHash = this.getPreviousHash();
+    block.index = this.getLength();
+    block.hash = block.createHash();
     this.chain.push(block);
+  }
+
+  isChainValid(){
+      for( let i = 1; i < this.getLength(); i++){
+        var currentBlock = this.chain[i];
+        var previousBlock = this.chain[i-1];
+
+        if(currentBlock.createHash() != currentBlock.hash){
+          return false;
+        }
+        if(currentBlock.previousHash != previousBlock.hash){
+          return false;
+        }
+      }
+      return true;
   }
 }
 
-let myCoin = new BlockChain
-myCoin.addBlock( { fromAccount: 'Tom', toAccount: 'Hank', amount: '20$' } )
-console.log(JSON.stringify(myCoin, null, 4))
+let myCoin = new BlockChain;
+myCoin.addBlock( new Block({ fromAccount: 'Tom', toAccount: 'Hank', amount: '20$' }) );
+myCoin.addBlock( new Block({ fromAccount: 'Lee', toAccount: 'Jason', amount: '100$' }) );
+console.log(JSON.stringify(myCoin, null, 4));
+console.log("Is my block chain valid: "+myCoin.isChainValid());
+console.log();
+console.log("Starting tampering ...");
+myCoin.chain[1].transaction = { fromAccount: 'Tom', toAccount: 'Hank', amount: '420$' };
+console.log(JSON.stringify(myCoin, null, 4));
+console.log("Is my block chain valid: "+myCoin.isChainValid());
